51bc319003e1d4a7c4a1834587402b4543c90298
itfat <itisfor@outlook.com>
2022-03-06 20:53:07 +0500

    crud completed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from asyncio import Task
 from cProfile import label
-from flask import Flask, url_for, render_template, redirect, flash, request
+from flask import Flask, url_for, render_template, redirect, flash, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey, PickleType
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
@@ -22,25 +22,24 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 class Users(UserMixin, db.Model):
-    id = db.Column('user_id', db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     password_hash = db.Column(db.String(100))
-    # tasks = db.relationship('tasks', backref='user', lazy=True)
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self,password):
       return check_password_hash(self.password_hash,password)
+    
 
 class Tasks(db.Model):
-    id = id = db.Column('task_id', db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable=False)
     priority = db.Column(db.String(100), nullable=False)
     labels = db.Column(MutableList.as_mutable(PickleType),
                                     default=[])
-    # user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey(Users.id))
 
 @app.before_first_request
 def create_tables():
@@ -49,10 +48,11 @@ def create_tables():
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/home')
 def home():
-    
     return render_template('home.html')
+
 
 
 @login_manager.user_loader
@@ -72,39 +72,82 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(email = form.email.data).first()
         if user is not None and user.check_password(form.password.data):
             login_user(user)
+            session['current_user'] = user.username
+            session['user_available'] = True
             next = request.args.get("next")
             return redirect(next or url_for('home'))
         flash('Invalid email address or Password.')    
     return render_template('login.html', form=form)
 
-@app.route("/forbidden",methods=['GET', 'POST'])
-@login_required
-def protected():
-    return redirect(url_for('forbidden.html'))
+# @app.route("/forbidden",methods=['GET', 'POST'])
+# @login_required
+# def protected():
+#     return redirect(url_for('forbidden.html'))
 
 @app.route("/logout")
 # @login_required
 def logout():
     logout_user()
+    session.clear()
+    session['user_available'] = False
     return redirect(url_for('index'))
 
-@app.route('/create', methods=['GET', 'POST'])
-def add_task():
-    form = TaskForm()
-    if form.validate_on_submit():
-        task = Tasks(title =form.title.data, priority = form.priority.data, labels = form.labels.data)
-        db.session.add(task)
-        db.session.commit()
-        print(db.session.task)
-        return redirect(url_for('home'))
 
-    return render_template('task.html', form=form)
-    
+@app.route('/create', methods=['GET', 'POST'])
+def create_task():
+    if session['user_available']:
+        form = TaskForm()
+        us = Users.query.filter_by(username=db.session['current_user']).first()
+        task = Tasks(title =form.title.data, priority = form.priority.data, labels = form.labels.data)
+        if request.method == 'POST':
+            task = Tasks(title =form.title.data, priority = form.priority.data, labels = form.labels.data)
+            db.session.add(task)
+            db.session.commit()
+            return redirect(url_for('show_task'))
+        return render_template('task.html', form=form)
+    flash('User is not Authenticated')
+    return redirect(url_for('index'))
+
+
+@app.route('/tasks')
+def show_task():
+    if 'user_available' in session:
+        tasks = Tasks.query.all()
+        user = Users.query.all()
+        return render_template('tasks.html', tasks=tasks, user=user)
+    flash('User is not Authenticated')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>/<user>', methods=('GET', 'POST'))
+def delete_post(id, user):
+    if db.session['current_user'] == user:
+        me = Tasks.query.get(id)
+        db.session.delete(me)
+        db.session.commit()
+        return redirect(url_for('show_posts'))
+    flash('You are not a valid user to delete this task')
+    return redirect(url_for('show_posts'))
+
+
+@app.route('/update/<id>/<user>', methods=('GET', 'POST'))
+def update_post(id, user):
+    if session['current_user'] == user:
+        me = Tasks.query.get(id)
+        task = TaskForm(obj=me)
+        if request.method == 'POST':
+            stask = Tasks.query.get(id)
+            stask.title = stask.title.data
+            stask.description = stask.description.data
+            db.session.commit()
+            return redirect(url_for('show_posts'))
+        return render_template('update.html', task=task)
+    flash('You are not a valid user to Edit this Post')
+    return redirect(url_for('show_posts'))
+
 if __name__ == '__main__':
    app.run(debug = True)
